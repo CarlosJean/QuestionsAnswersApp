5102eb7297fe65535569ecd10e322a3ddd5578af
Jean Holguin <holguinjean1@gmail.com>
2022-09-19 15:10:51 -0400

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace QuestionsAnswersApp.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
